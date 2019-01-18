d1edf5c9a066f8a65c06c80f38f99ec6732062d7
SaberStrat <k.sybin@gmx.net>
2019-01-18 23:14:20 +0100

    initial exporting of botcommands

---
+++ botcommands.js
@@ -0,0 +1,7 @@
+var cmds = {
+    ship(args){
+        ship.helptext=`A search on the official Star Citizen wiki, for the ship in the Query. 
+        _Example: !ship vulcan_`;
+        return args;
+    }
+}
